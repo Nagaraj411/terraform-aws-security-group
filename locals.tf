d5ac1ed6c95a9c00ae1001fbaf1365acbafcebfa
Nagaraj <nagarajuande255@gmail.com>
2025-06-23 23:25:35 +0530

    terraform

---
+++ locals.tf
@@ -0,0 +1,11 @@
+locals {
+  common_tags = {
+    Project     = var.project
+    Environment = var.environment
+    Terraform   = "true"
+  }
+
+  az_names = slice(data.aws_availability_zones.available.names, 0, 2) 
+  # Get the first two availability zones from the available AZs like us-east-1a, us-east-1b
+}
+
---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_security_group" "main" {
+    name = var.sg_name
+    description = var.sg_description
+    vpc_id = var.vpc_id
+
+    tags = merge(
+        var.sg_tags, # this allows other users to specify their own tags for the security group
+        locals.common_tags, # common tags defined in locals.tf
+        {
+            Name = "${var.project}-${var.environment}-${var.sg_name}" # Name of the security group roboshop-dev-frontend
+        }
+    )
+  
+}
