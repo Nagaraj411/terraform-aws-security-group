7cd08412961a1eea31361143c0416ea5d34ddf1e
Nagaraj <nagarajuande255@gmail.com>
2025-06-26 12:32:47 +0530

    terraform

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ resource "aws_security_group" "main" {
     description = var.sg_description
     vpc_id = var.vpc_id
 
+    egress = {
+        # Allow all outbound traffic
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1" # -1 means all protocols
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"] # Allow all IPv6 outbound traffic
+    }
+
     tags = merge(
         var.sg_tags, # this allows other users to specify their own tags for the security group
         local.common_tags, # common tags defined in locals.tf
